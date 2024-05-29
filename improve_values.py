cb7dc57b431fb76914e262025a1630da9701c26a
Miklos Parrag <miklos.parrag@ericsson.com>
2024-05-29 19:58:20 +0200

    value improver

--- improve_values.py
+++ improve_values.py
@@ -10,12 +10,12 @@ if __name__ == '__main__':
         csv_reader = csv.DictReader(file)
         checkpoints = [row for row in csv_reader]
 
+    c=0
     for checkpoint in checkpoints:
-        c=0
         with open(TARGET_FOLDER+FILE_TO_IMPROVE, "r+") as file:
             print(c)
             file_contents = file.read()
-            match = re.search(".*" + checkpoint['checkpoint'] + ".*verdict\.config\.field\.range\((\d+)\, (\d+)\).*",
+            match = re.search(".*" + checkpoint['checkpoint'] + ".*\n?.*verdict\.config\.field\.range\((\d+)\, (\d+)\).*",
                               file_contents)
             new_values = new_value_calc(
                 [checkpoint['new_value1'], checkpoint['new_value2'], checkpoint['new_value3']], match.group(1),
