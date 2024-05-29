4db1873d26fd7dc6f08351a6cdb2d8249ce51f46
Miklos Parrag <miklos.parrag@ericsson.com>
2024-05-29 17:12:33 +0200

    downloader

---
+++ epgcats_downloader.py
@@ -0,0 +1,13 @@
+# create a folder in the sandbox, and use that, and give: chmod -R 777 <folder>
+import subprocess
+
+TARGET_FOLDER = "/lab/epg_st_sandbox/erkmiap/PCEPGST-2857/"
+EPGCATS_VERSION = "EPG_28R252XS1"
+
+if __name__ == '__main__':
+    subprocess.call(
+        ["cp", "/lab/epg_scm4_builds/program/ci/" + EPGCATS_VERSION + "/epgcats", TARGET_FOLDER, "-r"])
+    subprocess.call(
+        ["cp", "/lab/epg_scm4_builds/program/ci/" + EPGCATS_VERSION + "/vipp/test-tools.info", TARGET_FOLDER])
+    subprocess.call(
+        ["mv", TARGET_FOLDER + "test-tools.info", TARGET_FOLDER + "paths"])
