21b80ac20063e9209a8212554f796486d9f57e8a
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-15 15:49:29 +0200

    collecting new values

--- 2-collect_new_values.py
+++ 2-collect_new_values.py
@@ -14,11 +14,10 @@ if __name__ == '__main__':
     for checkpoint in new_values.keys():
         with open(log1 + "/epg_testdata/" + result_value_file, "r+") as file:
             file_contents = file.read()
-            print(checkpoint)
             match = re.search(checkpoint + "\(?\w*\)?\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
             new_values[checkpoint].append(match.group(1))
 
     with open("configs/new_values.csv", "w") as file:
         file.write("checkpoint,new_value1")
         for value in new_values.keys():
-            file.write(value + ", " + new_values[value][0])
+            file.write(value + ", " + new_values[value][0] + "\n")
