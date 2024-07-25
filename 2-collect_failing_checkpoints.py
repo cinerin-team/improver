dc278c0ca15ae10eeb49488b0a2e4ec85099a268
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-25 15:24:35 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -4,19 +4,19 @@ from configs.variables import LOG1, LOG2, LOG3
 
 if __name__ == '__main__':
     new_fails = set()
-    with open(LOG1 + "/epg_testdata/verdict.log", "r+") as file:
+    with open(LOG1 + "/verdict.log", "r+") as file:
         file_contents = file.read()
         match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
         print("found: " + match.group(1))
         new_fails.add(match.group(1))
 
-    with open(LOG2 + "/epg_testdata/verdict.log", "r+") as file:
+    with open(LOG2 + "/verdict.log", "r+") as file:
         file_contents = file.read()
         match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
         print("found: " + match.group(1))
         new_fails.add(match.group(1))
 
-    with open(LOG3 + "/epg_testdata/verdict.log", "r+") as file:
+    with open(LOG3 + "/verdict.log", "r+") as file:
         file_contents = file.read()
         match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
         print("found: " + match.group(1))
--- 2-collect_new_values.py
+++ 2-collect_new_values.py
@@ -11,19 +11,19 @@ if __name__ == '__main__':
     f.close()
 
     for checkpoint in new_values.keys():
-        with open(LOG1 + RESULT_VALUE_FILE, "r+") as file:
+        with open(LOG1 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
             match = re.search(checkpoint + "\(?\w*\)?\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
             new_values[checkpoint].append(match.group(1))
 
     for checkpoint in new_values.keys():
-        with open(LOG2 + RESULT_VALUE_FILE, "r+") as file:
+        with open(LOG2 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
             match = re.search(checkpoint + "\(?\w*\)?\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
             new_values[checkpoint].append(match.group(1))
 
     for checkpoint in new_values.keys():
-        with open(LOG3 + RESULT_VALUE_FILE, "r+") as file:
+        with open(LOG3 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
             match = re.search(checkpoint + "\(?\w*\)?\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
             new_values[checkpoint].append(match.group(1))
@@ -31,7 +31,5 @@ if __name__ == '__main__':
     with open("configs/new_values.csv", "w") as file:
         file.write("checkpoint,new_value1,new_value2,new_value3\n")
         for value in new_values.keys():
-            print("found value: " + value + ", " + new_values[value][0] + "," + new_values[value][1] + "," +
-                  new_values[value][2])
             file.write(
                 value + ", " + new_values[value][0] + "," + new_values[value][1] + "," + new_values[value][2] + "\n")
