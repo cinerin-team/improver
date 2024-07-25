bc33349d34beaf7a5f34acbe9561efbdcf808ffb
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-25 15:32:50 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -6,21 +6,24 @@ if __name__ == '__main__':
     new_fails = set()
     with open(LOG1 + "/verdict.log", "r+") as file:
         file_contents = file.read()
-        match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
-        print("found: " + match.group(1))
-        new_fails.add(match.group(1))
+        match = re.findall(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
+        print("found: " + str(match))
+        for item in match:
+            new_fails.add(item)
 
     with open(LOG2 + "/verdict.log", "r+") as file:
         file_contents = file.read()
-        match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
-        print("found: " + match.group(1))
-        new_fails.add(match.group(1))
+        match = re.findall(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
+        print("found: " + str(match))
+        for item in match:
+            new_fails.add(item)
 
     with open(LOG3 + "/verdict.log", "r+") as file:
         file_contents = file.read()
-        match = re.search(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
-        print("found: " + match.group(1))
-        new_fails.add(match.group(1))
+        match = re.findall(".*218\s+(PDC.*)\sFailed\!", file_contents, re.MULTILINE)
+        print("found: " + str(match))
+        for item in match:
+            new_fails.add(item)
 
     with open("configs/checkpoints.csv", "w") as file:
         for item in new_fails:
