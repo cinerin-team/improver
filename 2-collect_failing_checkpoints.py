774349503ebe7235db9f9d3f60a11f09ebfc9c5b
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-26 13:14:50 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -26,12 +26,11 @@ def find_values(key, json_obj):
 if __name__ == '__main__':
     new_fails = set()
 
-    key_to_find = 'Failure Reason3'
+    key_to_find = 'Failure Reason'
 
     with open(LOG1 + "/verdict.json", "r+") as f:
         json_data = json.load(f)
     values = find_values(key_to_find, json_data)
-    print(values)
     for item1 in values:
 
         match = re.findall(".*31m(.*)\(\d+\.?\d+\) not inbetween.*", item1)
