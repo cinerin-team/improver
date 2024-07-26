679c4af020adfb1053927221c55461f7e40d990b
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-26 09:55:25 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -5,7 +5,8 @@ from configs.variables import LOG1, LOG2, LOG3
 
 if __name__ == '__main__':
     new_fails = set()
-    key_to_find = 'Failure Reason'
+    key_to_find = 'Failure Reason3'
+
 
     def find_values(key, json_obj):
         """Rekurzívan megkeresi a megadott kulcs értékeit a JSON struktúrában"""
@@ -36,27 +37,25 @@ if __name__ == '__main__':
         for item in match:
             new_fails.add(item)
 
+    with open(LOG2 + "/verdict.json", "r+") as f:
+        json_data = json.load(f)
+    values = find_values(key_to_find, json_data)
+    for item1 in values:
 
-    # with open(LOG1 + "/verdict.json", "r+") as file:
-    #     file_contents = file.read()
-    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
-    #     print("found: " + str(match))
-    #     for item in match:
-    #         new_fails.add(item)
+        match = re.findall(".*31m(.*)\(\d+\.?\d+\) not inbetween.*", item1)
+        print("found: " + str(match))
+        for item in match:
+            new_fails.add(item)
 
-    # with open(LOG2 + "/verdict.log", "r+") as file:
-    #     file_contents = file.read()
-    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
-    #     print("found: " + str(match))
-    #     for item in match:
-    #         new_fails.add(item)
-    #
-    # with open(LOG3 + "/verdict.log", "r+") as file:
-    #     file_contents = file.read()
-    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
-    #     print("found: " + str(match))
-    #     for item in match:
-    #         new_fails.add(item)
+    with open(LOG3 + "/verdict.json", "r+") as f:
+        json_data = json.load(f)
+    values = find_values(key_to_find, json_data)
+    for item1 in values:
+
+        match = re.findall(".*31m(.*)\(\d+\.?\d+\) not inbetween.*", item1)
+        print("found: " + str(match))
+        for item in match:
+            new_fails.add(item)
 
     with open("configs/checkpoints.csv", "w") as file:
         for item in new_fails:
