ddd603adf35f9284ba01cabccbe9d37901d4decd
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-26 13:54:24 +0200

    new values logging

--- 3-collect_new_values.py
+++ 3-collect_new_values.py
@@ -13,19 +13,25 @@ if __name__ == '__main__':
     for checkpoint in new_values.keys():
         with open(LOG1 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
-            match = re.search(checkpoint.replace("(","\(").replace(")","\)") + "\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
+            match = re.search(checkpoint.replace("(", "\(").replace(")", "\)") + "\:\s(\d+\.?\d*)", file_contents,
+                              re.MULTILINE)
+            print("found new value in LOG1 for: " + checkpoint + ": " + match.group(1))
             new_values[checkpoint].append(match.group(1))
 
     for checkpoint in new_values.keys():
         with open(LOG2 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
-            match = re.search(checkpoint.replace("(","\(").replace(")","\)") + "\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
+            match = re.search(checkpoint.replace("(", "\(").replace(")", "\)") + "\:\s(\d+\.?\d*)", file_contents,
+                              re.MULTILINE)
+            print("found new value in LOG2 for: " + checkpoint + ": " + match.group(1))
             new_values[checkpoint].append(match.group(1))
 
     for checkpoint in new_values.keys():
         with open(LOG3 + "/epg_testdata/" + RESULT_VALUE_FILE, "r+") as file:
             file_contents = file.read()
-            match = re.search(checkpoint.replace("(","\(").replace(")","\)") + "\:\s(\d+\.?\d*)", file_contents, re.MULTILINE)
+            match = re.search(checkpoint.replace("(", "\(").replace(")", "\)") + "\:\s(\d+\.?\d*)", file_contents,
+                              re.MULTILINE)
+            print("found new value in LOG3 for: " + checkpoint + ": " + match.group(1))
             new_values[checkpoint].append(match.group(1))
 
     with open("configs/new_values.csv", "w") as file:
