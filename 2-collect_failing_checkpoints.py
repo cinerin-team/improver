0063ed7324b3db301ea14f98e3c8e8df7db3cf23
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-26 13:12:34 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -3,33 +3,35 @@ import re
 
 from configs.variables import LOG1, LOG2, LOG3
 
+
+def find_values(key, json_obj):
+    """Rekurzívan megkeresi a megadott kulcs értékeit a JSON struktúrában"""
+    results = []
+
+    def _find_values(obj):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if k == key:
+                    results.append(v)
+                if isinstance(v, (dict, list)):
+                    _find_values(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                _find_values(item)
+
+    _find_values(json_obj)
+    return results
+
+
 if __name__ == '__main__':
     new_fails = set()
+
     key_to_find = 'Failure Reason3'
-
-
-    def find_values(key, json_obj):
-        """Rekurzívan megkeresi a megadott kulcs értékeit a JSON struktúrában"""
-        results = []
-
-        def _find_values(obj):
-            if isinstance(obj, dict):
-                for k, v in obj.items():
-                    if k == key:
-                        results.append(v)
-                    if isinstance(v, (dict, list)):
-                        _find_values(v)
-            elif isinstance(obj, list):
-                for item in obj:
-                    _find_values(item)
-
-        _find_values(json_obj)
-        return results
-
 
     with open(LOG1 + "/verdict.json", "r+") as f:
         json_data = json.load(f)
     values = find_values(key_to_find, json_data)
+    print(values)
     for item1 in values:
 
         match = re.findall(".*31m(.*)\(\d+\.?\d+\) not inbetween.*", item1)
