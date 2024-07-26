3493d0643bf254129af38a832a30700e03acbad5
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-26 09:36:05 +0200

    failing points collection

--- 2-collect_failing_checkpoints.py
+++ 2-collect_failing_checkpoints.py
@@ -1,29 +1,62 @@
+import json
 import re
 
 from configs.variables import LOG1, LOG2, LOG3
 
 if __name__ == '__main__':
     new_fails = set()
-    with open(LOG1 + "/verdict.log", "r+") as file:
-        file_contents = file.read()
-        match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
+    key_to_find = 'Failure Reason'
+
+    def find_values(key, json_obj):
+        """Rekurzívan megkeresi a megadott kulcs értékeit a JSON struktúrában"""
+        results = []
+
+        def _find_values(obj):
+            if isinstance(obj, dict):
+                for k, v in obj.items():
+                    if k == key:
+                        results.append(v)
+                    if isinstance(v, (dict, list)):
+                        _find_values(v)
+            elif isinstance(obj, list):
+                for item in obj:
+                    _find_values(item)
+
+        _find_values(json_obj)
+        return results
+
+
+    with open(LOG1 + "/verdict.json", "r+") as f:
+        json_data = json.load(f)
+    values = find_values(key_to_find, json_data)
+    for item1 in values:
+
+        match = re.findall(".*31m(.*)\(\d+\.?\d+\) not inbetween.*", item1)
         print("found: " + str(match))
         for item in match:
             new_fails.add(item)
 
-    with open(LOG2 + "/verdict.log", "r+") as file:
-        file_contents = file.read()
-        match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
-        print("found: " + str(match))
-        for item in match:
-            new_fails.add(item)
 
-    with open(LOG3 + "/verdict.log", "r+") as file:
-        file_contents = file.read()
-        match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
-        print("found: " + str(match))
-        for item in match:
-            new_fails.add(item)
+    # with open(LOG1 + "/verdict.json", "r+") as file:
+    #     file_contents = file.read()
+    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
+    #     print("found: " + str(match))
+    #     for item in match:
+    #         new_fails.add(item)
+
+    # with open(LOG2 + "/verdict.log", "r+") as file:
+    #     file_contents = file.read()
+    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
+    #     print("found: " + str(match))
+    #     for item in match:
+    #         new_fails.add(item)
+    #
+    # with open(LOG3 + "/verdict.log", "r+") as file:
+    #     file_contents = file.read()
+    #     match = re.findall(".*218\s+PDC.*\sFailed\!.*", file_contents)
+    #     print("found: " + str(match))
+    #     for item in match:
+    #         new_fails.add(item)
 
     with open("configs/checkpoints.csv", "w") as file:
         for item in new_fails:
