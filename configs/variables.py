8b4693f22202b210e59b3bb76868846f01ad5ecf
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-29 10:42:32 +0200

    new values logging

--- configs/variables.py
+++ configs/variables.py
@@ -4,6 +4,6 @@ FILE_TO_IMPROVE = "epgcats/tools/verdict/user_checkpoints/stability_checkpoints/
 REPO_FOLDER = '/workspace/git/erkmiap/epg/'
 ADDITIONAL_FILES_TO_COPY = ["epgcats/tcdb/TC_TID_general/TC_TID_stability.db"]  # like TC_TID_
 RESULT_VALUE_FILE = "stability_kpi.yml"
-LOG1 = "/lab/epg_st_portal_logs/EPG_28R253UZ1_240627_050354_31837171_erkmiap/2024-07-10_21.49_TC37540.1.6.16_Multi_Feature_LOCO_GWC_UPF_2_host_sriov_100mellanox_CX6_stability_ssr-vIP-Pt2"
+LOG1 = "/lab/epg_st_portal_logs/EPG_28R254JF1_240723_090345_31947513_erkmiap/2024-07-25_23.37_TC37540.1.6.16_Multi_Feature_LOCO_GWC_UPF_2_host_sriov_100mellanox_CX6_stability_ssr-vIP-Pt2"
 LOG2 = "/lab/epg_st_portal_logs/EPG_28R254JF1_240723_090345_31947513_erkmiap/2024-07-25_10.43_TC37540.1.6.16_Multi_Feature_LOCO_GWC_UPF_2_host_sriov_100mellanox_CX6_stability_ssr-vIP-Pt2"
 LOG3 = "/lab/epg_st_portal_logs/EPG_28R254JF1_240723_090345_31947513_erkmiap/2024-07-24_21.40_TC37540.1.6.16_Multi_Feature_LOCO_GWC_UPF_2_host_sriov_100mellanox_CX6_stability_ssr-vIP-Pt2"
